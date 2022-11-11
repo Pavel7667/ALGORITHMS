0ec5cd8aaaa06ff517dc2841d3395bc7f1a37466
pavel.hnelytsia <pavelhnelitcyagitlab@gmail.com>
2022-11-11 20:25:44 +0200

    O(I)

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -8,31 +8,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi ALGO");
-        int n = 100;
-        long[] memorization = new long[n + 1];
-        Arrays.fill(memorization, -1);
-        System.out.println(fibonacci(n, memorization));
+        System.out.println("log n > sqrt n > n > n log n > n^2 > 2^n");
+
     }
 
-    /**
-     * For 0 and 1 set n as 1, for another use Fibonacci
-     * formula. Using array for saving result of calc
-     *
-     * @param n Fibonacci number
-     * @param arr Fibonacci number
-     * @return Fibonacci count
-     */
-    // slow version -> optimization
-    public static long fibonacci(int n, long[] arr) {
-        if (arr[n] != -1) {
-            return arr[n];
-        }
-        if (n <= 1) {
-            return n;
-        }
-        long result = fibonacci(n - 1, arr) + fibonacci(n - 2, arr);
-        arr[n] = result;
-        return result;
-    }
 }
