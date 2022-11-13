c2404aadeb21f113e21489411647ccc9155ea35a
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 21:18:48 +0200

    Bubble_sort

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -1,51 +1,34 @@
 package org.algorithms;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("interpolation search");
+        System.out.println("Bubble_sort");
 
-        int[] array = new int[100000];
+        int[] array = {1, 2415, 567, 4564, 443242, 775, 453, 756745, 34, 64, 342, 7456};
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
-        }
-
-
-        int index = interpolationSearch(array, 45);
-        if (index != -1) {
-            System.out.println("Element in array = " + index);
-        } else {
-            System.out.println("Element not found");
-        }
 
     }
 
     /**
-     * Simple interpolationSearch O = log (log (n)) in best case
-     * In worst O (n)
+     * Simple bubbleSort O (n^2), may be good in small Data
+     * using double fori and one if statement, in case true swap digit
      *
      * @param array of digit
-     * @param value looking digit in array
-     * @return index of looking value or -1, if not found
      */
-    public static int interpolationSearch(int[] array, int value) {
-
-        int high = array.length - 1;
-        int low = 0;
-
-        while (value >= array[low] && value <= array[high] && low <= high) {
-            int probe = low + (high - low) *
-                    (value - array[low]) /
-                    (array[high] - array[low]);
-
-            if (array[probe] == value) {
-                return probe;
-            } else if (array[probe] < value) {
-                low = probe + 1;
-            } else {
-                low = probe - 1;
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
         }
-        return -1;
     }
 }
