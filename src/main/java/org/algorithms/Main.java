3330d1960a94eef901b38abe31c03a057606ac0c
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 20:57:45 +0200

    binarySearch

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -1,12 +1,19 @@
 package org.algorithms;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("linear search");
+        System.out.println("binary search");
 
-        int[] array = {1, 2, 3, 45, 6, 7, 8, 9, 12, 9678, 678, 5675, 57657};
+        int[] array = new int[100000];
 
-        int index = linerSearch(array, 45);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+
+        int index = Arrays.binarySearch(array, 45);
         if (index != -1) {
             System.out.println("Element in array = " + index);
         } else {
@@ -15,19 +22,4 @@ public class Main {
 
     }
 
-    /**
-     * Simple linerSearch
-     *
-     * @param array of digit
-     * @param value looking digit in array
-     * @return value or -1 if not found
-     */
-    public static int linerSearch(int[] array, int value) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                return i;
-            }
-        }
-        return -1;
-    }
 }
