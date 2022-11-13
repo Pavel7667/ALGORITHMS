4f330aa7c85b191aed96bad2c288478b71922074
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 21:44:49 +0200

    factorial calculation with recursion

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -1,35 +1,23 @@
 package org.algorithms;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Insertion sort");
 
-        int[] array = {1, 2415, 567, 4564, 443242, 775, 453, 756745, 34, 64, 342, 7456};
-        System.out.println(Arrays.toString(array));
-        insertionSort(array);
-        System.out.println(Arrays.toString(array));
-
+        int sum = factorial(3);
+        System.out.println(sum);
 
     }
 
     /**
-     * Simple insertionSort O (n^2), may be good in small Data
-     * (better than bubbleSort)
+     * Make factorial calculation with recursion
      *
-     * @param array of digit
+     * @param num of factorial
+     * @return sum of factorial
      */
-    public static void insertionSort(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            int temp = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j] > temp) {
-                array[j + 1] = array[j];
-                j--;
-            }
-            array[j+1] = temp;
-        }
+    private static int factorial(int num) {
+        if (num < 1) return 1;
+        System.out.println("This is factorial in Recursion " + num);
+        return num * factorial(num - 1);
     }
+
 }
