574c347ea49534a007e5d8345165fa53a92c1daf
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 21:08:50 +0200

    interpolationSearch

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -1,10 +1,8 @@
 package org.algorithms;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("binary search");
+        System.out.println("interpolation search");
 
         int[] array = new int[100000];
 
@@ -13,7 +11,7 @@ public class Main {
         }
 
 
-        int index = Arrays.binarySearch(array, 45);
+        int index = interpolationSearch(array, 45);
         if (index != -1) {
             System.out.println("Element in array = " + index);
         } else {
@@ -22,4 +20,32 @@ public class Main {
 
     }
 
+    /**
+     * Simple interpolationSearch O = log (log (n)) in best case
+     * In worst O (n)
+     *
+     * @param array of digit
+     * @param value looking digit in array
+     * @return index of looking value or -1, if not found
+     */
+    public static int interpolationSearch(int[] array, int value) {
+
+        int high = array.length - 1;
+        int low = 0;
+
+        while (value >= array[low] && value <= array[high] && low <= high) {
+            int probe = low + (high - low) *
+                    (value - array[low]) /
+                    (array[high] - array[low]);
+
+            if (array[probe] == value) {
+                return probe;
+            } else if (array[probe] < value) {
+                low = probe + 1;
+            } else {
+                low = probe - 1;
+            }
+        }
+        return -1;
+    }
 }
