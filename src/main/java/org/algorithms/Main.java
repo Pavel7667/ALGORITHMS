56ec349b528a8c528a3295eddda17a69c66db5bc
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 21:28:01 +0200

    selectionSort

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -4,31 +4,32 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bubble_sort");
+        System.out.println("Selection_sort");
 
         int[] array = {1, 2415, 567, 4564, 443242, 775, 453, 756745, 34, 64, 342, 7456};
         System.out.println(Arrays.toString(array));
-        bubbleSort(array);
+        selectionSort(array);
         System.out.println(Arrays.toString(array));
 
 
     }
 
     /**
-     * Simple bubbleSort O (n^2), may be good in small Data
-     * using double fori and one if statement, in case true swap digit
+     * Simple selectionSort O (n^2), may be good in small Data
      *
      * @param array of digit
      */
-    public static void bubbleSort(int[] array) {
+    public static void selectionSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
                 }
             }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
         }
     }
 }
