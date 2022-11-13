f6f2afaa1cfa5aafade191ecbaded4cacce3727f
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-13 21:33:58 +0200

    insertionSort

--- src/main/java/org/algorithms/Main.java
+++ src/main/java/org/algorithms/Main.java
@@ -4,32 +4,32 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Selection_sort");
+        System.out.println("Insertion sort");
 
         int[] array = {1, 2415, 567, 4564, 443242, 775, 453, 756745, 34, 64, 342, 7456};
         System.out.println(Arrays.toString(array));
-        selectionSort(array);
+        insertionSort(array);
         System.out.println(Arrays.toString(array));
 
 
     }
 
     /**
-     * Simple selectionSort O (n^2), may be good in small Data
+     * Simple insertionSort O (n^2), may be good in small Data
+     * (better than bubbleSort)
      *
      * @param array of digit
      */
-    public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[min] > array[j]) {
-                    min = j;
-                }
-            }
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
         }
     }
 }
