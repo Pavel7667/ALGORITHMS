4e4c4958fa9c1aec60ce4318b721a33d50de255e
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-24 15:03:55 +0200

    find Sum
    -findSumAlgo 1 millisecond
    -findSumLoop 4 millisecond

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double now = System.currentTimeMillis();
+        Main main = new Main();
+
+        System.out.println(main.findSumLoop(999999));
+
+        System.out.println("time taken " + (System.currentTimeMillis()-now)+" millisecond");
     }
+
+    private int findSumAlgo(int i) {
+        return i * (i + 1) / 2;
+    }
+
+    private int findSumLoop(int i) {
+        int sum = 0;
+        for (int j = 0; j < i; j++) {
+            sum = sum + j;
+        }
+        return sum;
+    }
+
 }
