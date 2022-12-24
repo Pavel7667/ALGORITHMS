8618fa1e62b1255f0fde2dadbefec97fa8efc486
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-24 15:51:23 +0200

    Reverse static array[]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(removeEven(arr)));
+        reverseArray(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
 
     }
 
@@ -27,5 +29,15 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public static void reverseArray(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
